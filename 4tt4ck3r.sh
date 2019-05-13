9334625bc1f86421b85b51017567afe922539bf0
Mr.Paint <50089388+PaintIsPainting@users.noreply.github.com>
2019-05-13 21:53:04 +0800

    Add files via upload

---
+++ 4tt4ck3r.sh
@@ -0,0 +1,239 @@
+#usr/bin/bash
+
+#Recode?
+#LuMauRecode ?
+#MatiLuDuluAnying
+clear
+
+lightgreen='\033[1;32m'
+i='\033[0;34'
+bi='\033[0;34m'
+okegreen='\033[92m'
+me='\033[1;31m'
+ku='\033[1;33m'
+pu='\033[1;37m'
+cyan='\033[0;36m'
+pur='\033[35;1m'
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $me"-.Sdh Nemu Yg D'cari?? "
+sleep 1
+echo $bi"-.Sorry Lw Kurang Lengkap"
+sleep 1
+echo $ku"-.Gua Hny Manusia Biasa"
+sleep 1
+echo $cyan"-.bye-bye:)"
+sleep 1
+clear
+exit
+}
+
+echo
+echo
+echo
+echo $me" Nama?? "
+read name
+echo " WellCome" $name
+echo ""
+echo $ku" Save Our Family Please "
+echo $ku" Our Family At Falestine "
+echo $ku" We Will Cry With Them "
+echo $ku" And Next, We Will Laugh With Them "
+echo ""
+echo ""
+echo $bi"["$ku"᠅"$bi"]"$bi"----["$ku"᠅"$bi"]---------["$ku"᠅"$bi"]-----------------------------"$bi"["$ku"᠅"$bi"]"
+echo $bi" |"$me" ["$ku"No"$me"] "$bi"|"$me" ["$ku"Ket."$me"]    "$bi"|"$me" ["$ku"Official"$me"]                   "$bi" |"
+echo $bi"["$ku"᠅"$bi"]"$bi"----["$ku"᠅"$bi"]---------["$ku"᠅"$bi"]-----------------------------"$bi"["$ku"᠅"$bi"]"
+echo $bi" |"$ku"  ——  "$bi"|"$ku" Author    :"$me" Mr.Paint                     "$bi" |"
+echo $bi" |"$ku"  ——  "$bi"|"$ku" Feat      :"$me" ⫷ᴍʜ.ʀᴇɢᴀ⫸                    "$bi" |"
+echo $bi" |"$ku"  ——  "$bi"|"$ku" Work With :"$me" |FCM|FRIEND CYBER MUSLIM     "$bi" |"
+echo $bi" |"$ku"  ——  "$bi"|"$ku" Contack WA:"$cyan" 082293825257                 "$bi" |"
+echo $bi" |"$ku"  ——  "$bi"|"$ku" NB        :"$me" Tool Khusus Attackking       "$bi" |"
+echo $bi" |"$ku"  ——  "$bi"|"$ku" Thanks To :"$me" All Cyber Indoneia           "$bi" |"
+echo $bi" |"$ku"  ——  "$bi"|"$ku" Pesan     :"$me" No Bokep. Otaklu D'Jaga      "$bi" |"
+echo $bi"["$ku"᠅"$bi"]"$bi"----["$ku"᠅"$bi"]---------["$ku"᠅"$bi"]-----------------------------"$bi"["$ku"᠅"$bi"]"
+echo $bi" |"$ku"  ——  "$bi"|"$ku" Special   :"$me" Allah SWT                    "$bi" |"
+echo $bi" |"$ku"  ——  "$bi"|"$ku" Takbir    :"$me" ALLAH HUAKBAR                "$bi" |"
+echo $bi"["$ku"᠅"$bi"]"$bi"----["$ku"᠅"$bi"]---------["$ku"᠅"$bi"]-----------------------------"$bi"["$ku"᠅"$bi"]"
+echo ""
+figlet -f small " Don't Forget to Pray For Palestine " | lolcat
+echo $cyan" Pilih Boss "
+echo $bi"["$ku"᠅"$bi"]"$bi"----["$ku"᠅"$bi"]-------["$ku"᠅"$bi"]-----------------------------"$bi"["$ku"᠅"$bi"]"
+echo $bi" |"$me" ["$ku"No"$me"] "$bi"|"$me" ["$ku"Jenis"$me"] "$bi"|"$me" ["$ku"NamaTool"$me"]                   "$bi" |"
+echo $bi"["$ku"᠅"$bi"]"$bi"----["$ku"᠅"$bi"]-------["$ku"᠅"$bi"]-----------------------------"$bi"["$ku"᠅"$bi"]"
+echo $bi" |"$me"   1  "$bi"|"$ku"         "$bi"|"$me" DoS-TOOKIT                   "$bi" |"
+echo $bi" |"$me"   2  "$bi"|"$ku"         "$bi"|"$me" Hammer                       "$bi" |"
+echo $bi" |"$me"   3  "$bi"|"$ku"    D    "$bi"|"$me" Xerxes                       "$bi" |"
+echo $bi" |"$me"   4  "$bi"|"$ku"    D    "$bi"|"$me" Hulk                         "$bi" |"
+echo $bi" |"$me"   5  "$bi"|"$ku"    O    "$bi"|"$me" ILuck                        "$bi" |"
+echo $bi" |"$me"   6  "$bi"|"$ku"    S    "$bi"|"$me" Torshammer                   "$bi" |"
+echo $bi" |"$me"   7  "$bi"|"$ku"         "$bi"|"$me" DDoSAttacks                  "$bi" |"
+echo $bi" |"$me"   9  "$bi"|"$ku"         "$bi"|"$me" LITEDDOS                     "$bi" |"
+echo $bi" |"$me"  10  "$bi"|"$ku"         "$bi"|"$me" slowloris                    "$bi" |"
+echo $bi"["$ku"᠅"$bi"]"$bi"----["$ku"᠅"$bi"]-------["$ku"᠅"$bi"]-----------------------------"$bi"["$ku"᠅"$bi"]"
+echo $bi" |"$me"  00  "$bi"|"$ku"  exit   "$bi"|"$ku" Keluar                       "$bi" |"
+echo $bi"["$ku"᠅"$bi"]"$bi"----["$ku"᠅"$bi"]-------["$ku"᠅"$bi"]-----------------------------"$bi"["$ku"᠅"$bi"]"
+echo ""
+echo $bi" Tool Masih Dlam Rekonstruksi"$ku","$pur"Tunggu Update Eaa"
+read -p"Gas-Keuunn >" painting
+
+if [ $painting = 1 ] || [ $painting = 1 ]
+then
+clear
+figlet -f small " From Us 2 U " | lolcat
+echo $me" Load The Tool "
+sleep 1
+apt update
+apt upgrade
+cd
+git clone https://github.com/BPCATTACKER/DOS-TOOKITS
+cd DOS-TOOKITS
+sh DoS-TOOKIT.sh
+echo $cyan" Saving The Tool "
+fi
+
+if [ $painting = 2 ] || [ $painting = 2 ]
+then
+clear
+figlet -f small " From Us 2 U " | lolcat
+echo $me" Load The Tool "
+sleep 1
+apt update
+apt upgrade
+cd
+git clone https://github.com/cyweb/hammer
+cd hammer
+chmod 7777 hammer.py
+echo $cyan" Saving The Tool "
+fi
+
+if [ $painting = 3 ] || [ $painting = 3 ]
+then
+clear
+figlet -f small " From Us 2 U " | lolcat
+echo $me" Load The Tool "
+sleep 1
+apt update
+apt upgrade
+cd
+apt install clang
+git clone https://github.com/zanyarjamal/xerxes
+cd xerxes
+clang xerxes.c -o xerxes
+./xerxes
+echo $cyan" Saving The Tool "
+fi
+
+if [ $painting = 4 ] || [ $painting = 4 ]
+then
+clear
+figlet -f small " From Us 2 U " | lolcat
+echo $me" Load The Tool "
+sleep 1
+apt update
+apt upgrade
+cd
+git clone https://github.com/ujjawalsaini3/hulk
+cd hulk
+ls
+echo $cyan" Saving The Tool "
+fi
+
+if [ $painting = 5 ] || [ $painting = 5 ]
+then
+clear
+figlet -f small " From Us 2 U " | lolcat
+echo $me" Load The Tool "
+sleep 1
+apt update
+apt upgrade
+cd png
+mv Iluck $HOME
+cd
+cd ATTACK
+rm -rf png
+cd
+cd Iluck
+ls
+echo $cyan" Saving The Tool "
+fi
+
+if [ $painting = 6 ] || [ $painting = 6 ]
+then
+clear
+figlet -f small " From Us 2 U " | lolcat
+echo $me" Load The Tool "
+sleep 1
+apt update
+apt upgrade
+cd
+apt install tor
+git clone httpvs://github.com/dotfighter/torshammer.git
+cd torshammera
+chmod 777 torshammer.py
+python2 torshammer.py
+echo $cyan" Saving The Tool "
+fi
+
+if [ $painting = 7 ] || [ $painting = 7 ]
+then
+clear
+figlet -f small " From Us 2 U " | lolcat
+echo $me" Load The Tool "
+sleep 1
+apt update
+apt upgrade
+cd
+git clone https://github.com/mrhidden13/DDoSAttacks.git
+cd DDoSAttacks
+python2 DDoS.py
+echo $cyan" Saving The Tool "
+fi
+
+if [ $painting = 8 ] || [ $painting = 8 ]
+then
+clear
+figlet -f small " From Us 2 U " | lolcat
+echo $me" Load The Tool "
+sleep 1
+apt update
+apt upgrade
+cd
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+ls
+echo $cyan" Saving The Tool "
+fi
+
+if [ $painting = 9 ] || [ $painting = 9 ]
+then
+clear
+figlet -f small " From Us 2 U " | lolcat
+echo $me" Load The Tool "
+sleep 1
+apt update
+apt upgrade
+cd
+git clone https://github.com/gkbrk/slowloris.git
+cd slowloris
+ls
+echo $cyan" Saving The Tool "
+fi
+
+if [ $painting = 00 ] || [ $painting = 00 ]
+then
+clear
+figlet -f slant " Wanna GO ?? " | lolcat
+sleep 1
+echo $ku" Bye-Bye "
+sleep 1
+echo $me" Salam Sama Cewekmu "
+sleep 1
+clear
+exit
+fi
